94bddc3846cf7baeb3087826b280628a016b5927
Jithaamithran S <72763591+SJM2501@users.noreply.github.com>
2021-04-23 06:20:55 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,28 +1,39 @@
 import socket,pickle,struct,time
-from cv2 import cv2
-SS=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-hostname=socket.gethostname()
-ip=socket.gethostbyname(hostname)
-print(hostname,ip)
-PORT=2501
-SA=(ip,PORT)
-SS.bind(SA)
-SS.listen(5)
-pt=0
-while True:
-    cl_socket,addr=SS.accept()
-    print("CONNECTED TO",addr)
-    if cl_socket:
-        cap=cv2.VideoCapture(0)
-        suc,img=cap.read()
-        ct=time.time()
-        fps=1/(ct-pt)
-        pt=ct
-        cv2.putText(img,str(int(fps)),(10,20),cv2.FONT_HERSHEY_COMPLEX,2,(255,0,0),3)
-        a=pickle.dumps(img)
-        msg=struct.pack("Q",len(a))+a
-        cl_socket.sendall(msg)
-        cv2.imshow("VIDEO",img)
-        if cv2.waitKey(1) & 0xFF==ord('q'):
-            cl_socket.close()
 
+from cv2 import cv2
+
+
+cl_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+host_ip='26.126.177.24'
+port=25010
+k=(host_ip,port)
+cl_socket.connect(k)
+
+data=b''
+payload_size=struct.calcsize("Q")
+
+while True:
+    while len(data)<payload_size:
+        packet=cl_socket.recv(512)
+        if not packet:break
+        data+=packet
+
+    packed_msg=data[:payload_size]
+    dat=data[payload_size:]   
+    msg_size=struct.unpack("Q",packed_msg)[0]
+
+
+    while len(dat)<msg_size:
+
+        data+=cl_socket.recv(512)
+
+    frame=data[:msg_size]
+
+    data=data[msg_size:]
+    print("work")
+    frame=pickle.loads(frame)
+
+    cv2.imshow("out",frame)
+    
+cl_socket.close()
